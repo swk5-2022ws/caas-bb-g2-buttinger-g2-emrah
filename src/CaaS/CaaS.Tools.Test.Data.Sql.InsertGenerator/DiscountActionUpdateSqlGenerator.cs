f3ab3f5e0a2fe29c6398c12eb236c62968593e53
Buttinger Xaver <xaver.buttinger@gmail.com>
2022-11-11 12:45:06 +0100

    implement sql test data generator DiscountAction and DiscountRule

---
+++ src/CaaS/CaaS.Tools.Test.Data.Sql.InsertGenerator/DiscountActionUpdateSqlGenerator.cs
@@ -0,0 +1,41 @@
+﻿using CaaS.Core.Domainmodels.DiscountActions;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaaS.Tools.Test.Data.Sql.InsertGenerator
+{
+    internal class DiscountActionUpdateSqlGenerator
+    {
+        readonly Random random = new();
+
+        internal IEnumerable<string> GenerateActionsForExistingDiscountActions()
+        {
+            int count = 200;
+            return GenerateDiscountActionsUpdateStatement(count);
+        }
+
+        private IEnumerable<string> GenerateDiscountActionsUpdateStatement(int count)
+        {
+            List<string> discountActions = new List<string>();
+            for (int i = 1; i <= count; i++)
+            {
+                int type = i % 2;
+                DiscountActionBase? action = null;
+                action = type switch
+                {
+                    0 => new FixedValueDiscountAction(random.Next(5, 100)),
+                    1 => new TotalPercentageDiscountAction(random.NextDouble()),
+                    _ => throw new ArgumentOutOfRangeException($"type {type} not implemented."),
+                };
+                string json = DiscountActionBase.Serialize(action);
+
+                string sql = $"UPDATE DiscountAction SET Action = '{json}' WHERE Id = {i};";
+                yield return sql;
+            }
+        }
+    }
+}
---
+++ src/CaaS/CaaS.Tools.Test.Data.Sql.InsertGenerator/DiscountRuleUpdateSqlGenerator.cs
@@ -0,0 +1,47 @@
+﻿using CaaS.Core.Domainmodels.DiscountActions;
+using CaaS.Core.Domainmodels.DiscountRules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaaS.Tools.Test.Data.Sql.InsertGenerator
+{
+    internal class DiscountRuleUpdateSqlGenerator
+    {
+         readonly Random random = new();
+
+        internal IEnumerable<string> GenerateRulesForExistingDiscountRules()
+        {
+            int count = 100;
+            return GenerateDiscountRuleUpdateStatement(count);
+        }
+
+        private IEnumerable<string> GenerateDiscountRuleUpdateStatement(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                int type = i % 2;
+
+                DiscountRulesetBase action = type switch
+                {
+                    0 => GenerateDateDiscountRuleset(),
+                    1 => new TotalAmountDiscountRuleset(random.Next(5, 800)),
+                    _ => throw new ArgumentOutOfRangeException($"type {type} not implemented."),
+                };
+                string json = DiscountRulesetBase.Serialize(action);
+
+                string sql = $"UPDATE DiscountRule SET Ruleset = '{json}' WHERE Id = {i};";
+                yield return sql;
+            }
+        }
+
+        private DiscountRulesetBase GenerateDateDiscountRuleset()
+        {
+            int monthBeginDelta = random.Next(0, 12);
+            int monthEndDelta = random.Next(monthBeginDelta, 24);
+            return new DateDiscountRuleset(DateTime.Now.AddMonths(-monthBeginDelta), DateTime.Now.AddDays(monthEndDelta), DateTime.Now);
+        }
+    }
+}
