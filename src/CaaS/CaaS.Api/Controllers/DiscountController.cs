ab473b05eee4f484c8e7b841ce1305130d2ec0ea
Buttinger Xaver <xaver.buttinger@gmail.com>
2022-12-19 20:22:41 +0100

    Modify exception handling

--- src/CaaS/CaaS.Api/Controllers/DiscountController.cs
+++ src/CaaS/CaaS.Api/Controllers/DiscountController.cs
@@ -43,14 +43,18 @@ namespace CaaS.Api.Controllers
 
                 return Ok(tDiscounts);
             }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Unauthorized();
+            }
             catch (Exception ex)
                 when (ex is UnauthorizedAccessException || ex is ArgumentException)
             {
                 return BadRequest(ex.Message);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                return NotFound(ex.Message);
             }
         }
 
@@ -60,17 +64,22 @@ namespace CaaS.Api.Controllers
         {
             try
             {
+                // TODO test
                 await discountLogic.AddDiscountsToCart(appKey, id, discountIds);
                 return NoContent();
-            }
-            catch (Exception ex)
-                when (ex is UnauthorizedAccessException || ex is ArgumentException)
-            {
-                return BadRequest(ex.Message);
             }
             catch (KeyNotFoundException ex)
             {
                 return NotFound(ex.Message);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Unauthorized();
+            }
+            catch (Exception ex)
+                when (ex is ArgumentException || ex is ArgumentNullException)
+            {
+                return BadRequest(ex.Message);
             }
         }
 
