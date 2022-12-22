09f61f11b7808015f089079fcb36dd266c7601ea
Buttinger Xaver <xaver.buttinger@gmail.com>
2022-12-22 18:01:58 +0100

    CustomerLogic

---
+++ src/CaaS/CaaS.Core/Interfaces/Logic/ICustomerLogic.cs
@@ -0,0 +1,37 @@
+﻿using CaaS.Core.Domainmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaaS.Core.Interfaces.Logic
+{
+    public interface ICustomerLogic
+    {
+        /// <summary>
+        /// Get all customers for a shop.
+        /// </summary>
+        Task<IEnumerable<Customer>> GetByShopId(Guid appKey, int shopId);
+
+        /// <summary>
+        /// Gets a customer.
+        /// </summary
+        Task<Customer> Get(Guid appKey, int id);
+
+        /// <summary>
+        /// Creates a customer.
+        /// </summary>
+        Task<int> Create(Guid appKey, Customer customer);
+
+        /// <summary>
+        /// Updates a customer.
+        /// </summary>
+        Task<bool> Update(Guid appKey, Customer customer);
+
+        /// <summary>
+        /// Deletes a customer.
+        /// </summary>        
+        Task<bool> Delete(Guid appKey, int id);
+    }
+}
---
+++ src/CaaS/CaaS.Core/Logic/CustomerLogic.cs
@@ -0,0 +1,58 @@
+﻿using CaaS.Core.Domainmodels;
+using CaaS.Core.Interfaces.Logic;
+using CaaS.Core.Interfaces.Repository;
+using CaaS.Core.Logic.Util;
+using CaaS.Core.Repository;
+using CaaS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaaS.Core.Logic
+{
+    public class CustomerLogic : ICustomerLogic
+    {
+        private readonly ICustomerRepository customerRepository;
+        private readonly IShopRepository shopRepository;
+
+        public CustomerLogic(ICustomerRepository customerRepository, IShopRepository shopRepository)
+        {
+            this.customerRepository = customerRepository;
+            this.shopRepository = shopRepository;
+        }
+
+        public async Task<int> Create(Guid appKey, Customer customer)
+        {
+            if (customer is null) throw ExceptionUtil.ParameterNullException(nameof(customer));
+            var customerId = await customerRepository.Create(customer);
+            return customerId;
+        }
+
+        public async Task<bool> Delete(Guid appKey, int id)
+        {
+            await Check.Customer(shopRepository, customerRepository, id, appKey);
+            return await customerRepository.Delete(id);
+        }
+
+        public async Task<Customer> Get(Guid appKey, int id)
+        {
+            await Check.Customer(shopRepository, customerRepository, id, appKey);
+            return await customerRepository.Get(id) ?? throw ExceptionUtil.NoSuchIdException(nameof(id));
+        }
+
+        public async Task<IEnumerable<Customer>> GetByShopId(Guid appKey, int shopId)
+        {
+            await Check.Shop(shopRepository, shopId, appKey);
+            return await customerRepository.GetAllByShopId(shopId);
+        }
+
+        public async Task<bool> Update(Guid appKey, Customer customer)
+        {
+            if (customer is null) throw ExceptionUtil.ParameterNullException(nameof(customer));
+            await Check.Customer(shopRepository, customerRepository, customer.Id, appKey);
+            return await customerRepository.Update(customer);
+        }
+    }
+}
